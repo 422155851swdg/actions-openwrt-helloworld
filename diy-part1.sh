fd99aca7e18c3c1bbd1f9bd0a7d3da2c448a9fc9
422155851swdg <96711188+422155851swdg@users.noreply.github.com>
2022-01-09 21:05:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,8 +13,8 @@
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git packages https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git passwall https://github.com/kenzok8/small' >>feeds.conf.default
 
 # Lienol/openwrt-package
 # sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
